9a7cf35ad46107f066d4d8ac5747207cbad1d12d
TimMark <noierrdev@proton.me>
2023-11-16 05:51:05 -0800

    routes of api commented

--- routes/api.php
+++ routes/api.php
@@ -21,15 +21,18 @@ use App\Http\Middleware\IsAdmin;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//API-AUTH
 Route::controller(ApiController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
 });
+//API-Transactions
 Route::prefix('transactions')->middleware('auth:sanctum')->group(function (){
     Route::post('/save',[ApiController::class,'saveTransaction'])->middleware('IsAdmin')->name('api.transactions.save');
-//    Route::post('/{id}/add-payment',[ApiController::class,'savePayment'])->middleware('IsAdmin')->name('api.payments.save');
     Route::get('/{id}',[ApiController::class,'getTransaction'])->name('api.transactions.get');
 });
+//API-Payments
 Route::post('/payments/save',[ApiController::class,'savePayment'])->middleware('auth:sanctum','IsAdmin')->name('api.payments.save');
+//API-Report
 Route::post('/research',[ApiController::class,'makeResearch'])->middleware('IsAdmin')->name('api.transactions.get');
